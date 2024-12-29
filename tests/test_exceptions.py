e1850149291c2ccb070bc03e761768ac06d7bdd5
Damian Kapczynski <damkapp2@gmail.com>
2024-12-29 14:22:46 +0100

    Adding TimeoutException test

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -56,6 +56,7 @@ class TestExceptionsScenarios:
         confirmation_locator = wait.until(ec.visibility_of_element_located((By.ID, "confirmation")))
         assert confirmation_locator.text == "Row 1 was saved", "Wrong confirmation message"
 
+
     def test_StaleElementReferenceException(self, driver):
         #Open page
         driver.get("https://practicetestautomation.com/practice-test-exceptions/")
@@ -70,3 +71,17 @@ class TestExceptionsScenarios:
         wait = WebDriverWait(driver, 10)
         assert wait.until(ec.invisibility_of_element_located((By.ID, 'instructions'))), "Instrucion text element is displayed but it shouldn't"
 
+
+    def test_TimeoutException(self, driver):
+        #Open page
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
+
+        #Click Add button
+        driver.find_element(By.ID, 'add_btn').click()
+
+        #Wait for 6 seconds for the second input field to be displayed
+        wait = WebDriverWait(driver, 6)
+        row2_input_locator = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")))
+
+        #Verify second input field is displayed
+        assert row2_input_locator.is_displayed(), "Row2 is not displayed but it should be"
