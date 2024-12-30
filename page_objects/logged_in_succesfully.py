ce2b17ca20f75e580d875cd2dc8102b5107f2021
Damian Kapczynski <damkapp2@gmail.com>
2024-12-30 19:29:15 +0100

    Adding Logged In Sucessfully Page Object

---
+++ page_objects/logged_in_succesfully.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class LoggedInSuccessfullyPage:
+    _url = "https://practicetestautomation.com/logged-in-successfully/"
+    __header_locator = (By.TAG_NAME, "h1")
+    __log_out_button_locator = (By.LINK_TEXT, "Log out")
+
+    def __int__(self, driver: WebDriver):
+        self._driver = driver
+
+    @property
+    def get_current_url(self) -> str:
+        return self._driver.current_url
+
+    @property
+    def get_expected_url(self) -> str:
+        return self._url
+
+    @property
+    def get_header(self) -> str:
+        return self._driver.find_element(self.__header_locator).text
+
+    def is_logout_button_displayed(self) -> bool:
+        return self._driver.find_element(__log_out_button_locator).is_displayed()
