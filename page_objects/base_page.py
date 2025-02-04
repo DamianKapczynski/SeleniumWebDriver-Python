bb1543aa51227413803fd10e9ff4216862dfc602
Damian Kapczynski <damkapp2@gmail.com>
2025-02-04 20:55:18 +0100

    Refactor InvalidElementStateException test

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -12,23 +12,27 @@ class BasePage:
         return self._driver.find_element(*locator)
 
     def _type(self, locator: tuple, text: str, time: int = 10):
-        self._wait_untill_element_is_visible(locator, time)
+        self._wait_until_element_is_visible(locator, time)
         self._find(locator).send_keys(text)
 
     def _click(self, locator: tuple, time: int = 10):
-        self._wait_untill_element_is_visible(locator, time)
+        self._wait_until_element_is_visible(locator, time)
         self._find(locator).click()
 
-    def _wait_untill_element_is_visible(self, locator, time: int = 10):
+    def _wait_until_element_is_visible(self, locator, time: int = 10):
         wait = WebDriverWait(self._driver, time)
         wait.until(ec.presence_of_element_located(locator))
+
+    def _wait_until_element_is_clickable(self, locator, time: int = 10):
+        wait = WebDriverWait(self._driver, time)
+        wait.until(ec.element_to_be_clickable(locator))
 
     @property
     def current_url(self) -> str:
         return self._driver.current_url
 
     def _is_displayed(self, locator: tuple, time: int = 10) -> bool:
-        self._wait_untill_element_is_visible(locator, time)
+        self._wait_until_element_is_visible(locator, time)
         try:
             return self._find(locator).is_displayed()
         except NoSuchElementException:
@@ -38,9 +42,9 @@ class BasePage:
         self._driver.get(url)
 
     def _get_text(self, locator: tuple, time: int = 10) -> str:
-        self._wait_untill_element_is_visible(locator, time)
+        self._wait_until_element_is_visible(locator, time)
         return self._find(locator).text
 
     def _clear_text(self, locator: tuple, time: int = 10):
-        self._wait_untill_element_is_visible(locator, time)
+        self._wait_until_element_is_visible(locator, time)
         return self._find(locator).clear()
--- page_objects/exceptions_page.py
+++ page_objects/exceptions_page.py
@@ -26,7 +26,7 @@ class ExceptionsPage(BasePage):
 
     def add_second_row(self):
         super()._click(self.__add_button)
-        super()._wait_untill_element_is_visible(self.__row_2_input_element)
+        super()._wait_until_element_is_visible(self.__row_2_input_element)
 
     def is_row_2_displayed(self):
         return super()._is_displayed(self.__row_2_input_element)
@@ -37,15 +37,16 @@ class ExceptionsPage(BasePage):
     def add_text_to_1_row(self):
         super()._type(self.__row_1_input_element, self.__row_1_input_text)
         super()._click(self.__row_1_save_button)
-        super()._wait_untill_element_is_visible(self.__confirmation_element)
+        super()._wait_until_element_is_visible(self.__confirmation_element)
 
     def add_text_to_2_row(self):
         super()._type(self.__row_2_input_element, self.__row_2_input_text)
         super()._click(self.__row_2_save_button)
-        super()._wait_untill_element_is_visible(self.__confirmation_element)
+        super()._wait_until_element_is_visible(self.__confirmation_element)
 
     def clear_text_1_row(self):
         super()._click(self.__edit_button)
+        super()._wait_until_element_is_clickable(self.__row_1_input_element)
         super()._clear_text(self.__row_1_input_element)
 
     def get_confirmation_message(self):
